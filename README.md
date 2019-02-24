bf6b1083f50cd786b8c75760c1ab60bce81b90a0
Thiasap <799721744@qq.com>
2019-02-24 21:22:45 +0800

    Rewrite

--- README.md
+++ README.md
@@ -1,6 +1,11 @@
 # SelectPic
 快速显示一张图片，用于展示常用而不好找的图片，简化过程
+## v1.2
+使用ViewPage(最后代码整到Mainactivity了)实现3张图片相互切换
+打开时展示上一次显示的图片
+更改图片选择器为知乎开源的[Matisse](https://github.com/zhihu/Matisse)
 
+<img src="https://github.com/Thiasap/MarkdownPhotos/raw/master/Video_20190224_082207_774.gif" width="200" align=center /><img src="https://github.com/Thiasap/MarkdownPhotos/raw/master/Video_20190224_082317_509.gif" width="200" align=center />
 
 ## v1.1
 更换ImageView为[PhotoView](https://github.com/bm-x/PhotoView)  
--- app/src/main/java/com/bistu747/selectpic/MainActivity.java
+++ app/src/main/java/com/bistu747/selectpic/MainActivity.java
@@ -1,69 +1,172 @@
 package com.bistu747.selectpic;
 
 import android.app.Activity;
-import android.content.ContentUris;
 import android.content.Context;
 import android.content.Intent;
 import android.content.SharedPreferences;
 import android.content.pm.PackageManager;
-import android.database.Cursor;
-import android.graphics.Bitmap;
-import android.graphics.BitmapFactory;
 import android.net.Uri;
-import android.os.Bundle;
-import android.provider.DocumentsContract;
-import android.provider.MediaStore;
 import android.support.v4.app.ActivityCompat;
+import android.support.v4.view.PagerAdapter;
+import android.support.v4.view.ViewPager;
 import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
 import android.util.Log;
 import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ImageView;
 import android.widget.TextView;
 import android.widget.Toast;
 
 import com.bm.library.PhotoView;
+import com.bumptech.glide.Glide;
+import com.zhihu.matisse.Matisse;
+import com.zhihu.matisse.MimeType;
 
-import java.io.File;
+import java.util.List;
 
 public class MainActivity extends AppCompatActivity {
-    private PhotoView photoView;
-    //获取sharedPreferences对象
-    private SharedPreferences sharedPreferences;
-    private String imgPath;
+    private String TAG = "ViewDebug";
+    private int MAX_PIC = 3;
+    private int MAX_SCALE = 4;
     private TextView FirstStart;
+    private SharedPreferences sharedPreferences;
+    private ViewPager mPager;
+    private Uri[] ImgPaths = new Uri[MAX_PIC];
+    private int lastPosition;
+    PhotoView[] view = new PhotoView[3];
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         init();
-
-
     }
-    void init() {
-        //获取sharedPreferences对象
+    void init(){
         sharedPreferences = getSharedPreferences("ImgPath", Context.MODE_PRIVATE);
-        FirstStart = (TextView) findViewById(R.id.FirstText);
-        photoView = (PhotoView) findViewById(R.id.Pic);
+        mPager = (ViewPager) findViewById(R.id.pager);
+        FirstStart = (TextView) findViewById(R.id.FirstStart);
+
         checkPermissions(MainActivity.this);
-        photoView.enable();// 启用图片缩放功能
-        photoView.setMaxScale(4);
-        photoView.setOnLongClickListener(new View.OnLongClickListener() {
-            @Override
-            public boolean onLongClick(View v) {
-                openAlbum();
-                return true;
-            }
-        });
         load();
     }
-    void load(){
-        imgPath = sharedPreferences.getString("Path", null);
-        if(fileIsExists(imgPath)){
-            //FirstStart.setVisibility(View.INVISIBLE);
-            displayImage(imgPath);
-        }else {
-            FirstStart.setVisibility(View.VISIBLE);
+    void load() {
+        for (int i = 0; i < 3; i++) {
+            String str = sharedPreferences.getString("Path" + i, null);
+            if (str!=null) ImgPaths[i] = Uri.parse((String) str);
+            else ImgPaths[i]=null;
+            view[i]= new PhotoView(MainActivity.this);
+            view[i].setOnLongClickListener(new View.OnLongClickListener() {
+                @Override
+                public boolean onLongClick(View v) {
+                    //Toast.makeText(ViewPagerActivity.this,"Long Click!",Toast.LENGTH_SHORT).show();
+                    //openAlbum();
+                    Matisse.from(MainActivity.this)
+                            .choose(MimeType.ofImage(), false)//图片类型
+                            .countable(false)//true:选中后显示数字;false:选中后显示对号
+                            .maxSelectable(1)//可选的最大数
+                            .capture(false)//选择照片时，是否显示拍照
+                            //.captureStrategy(new CaptureStrategy(true, "com.example.xx.fileprovider"))//参数1 true表示拍照存储在共有目录，false表示存储在私有目录；参数2与 AndroidManifest中authorities值相同，用于适配7.0系统 必须设置
+                            .thumbnailScale(0.87f)//缩略图的清晰程度(与内存占用有关)
+                            .imageEngine(new GlideLoadEngine())//图片加载引擎
+                            .forResult(1);//
+                    return true;
+                }
+            });
         }
+        lastPosition = sharedPreferences.getInt("lastPosition", 0);
+        if(ImgPaths[lastPosition]!=null){
+            FirstStart.setVisibility(View.INVISIBLE);
+            mlog(ImgPaths[lastPosition].toString());
+        }
+        else FirstStart.setVisibility(View.VISIBLE);
+        mlog("last position is :"+lastPosition);
+        mPager.setPageMargin((int) (getResources().getDisplayMetrics().density * 15));
+        mPager.setAdapter(new PagerAdapter() {
+            @Override
+            public int getCount() {
+                return ImgPaths.length;
+            }
 
+            @Override
+            public boolean isViewFromObject(View view, Object object) {
+                return view == object;
+            }
+
+            @Override
+            public Object instantiateItem(ViewGroup container, final int position) {
+                mlog( "now create position " + position);
+                view[position].enable();
+                view[position].setScaleType(ImageView.ScaleType.FIT_CENTER);
+                //view[position].setImageResource(imgsId[position]);
+                displayImage(ImgPaths[position],position);
+                view[position].setMaxScale(MAX_SCALE);
+                container.addView(view[position]);
+                return view[position];
+            }
+
+            @Override
+            public void destroyItem(ViewGroup container, int position, Object object) {
+                container.removeView((View) object);
+            }
+        });
+        mPager.setCurrentItem(lastPosition, false);
+        mPager.addOnPageChangeListener(new ViewPager.OnPageChangeListener() {
+            @Override
+            public void onPageScrolled(int position, float positionOffset, int positionOffsetPixels) {
+
+            }
+
+            @Override
+            public void onPageSelected(int position) {
+                lastPosition = position;
+                SharedPreferences.Editor editor = sharedPreferences.edit();
+                editor.putInt("lastPosition", position);
+                mlog("save position "+position);
+                editor.apply();
+                if(ImgPaths[position]!=null){
+                    FirstStart.setVisibility(View.INVISIBLE);
+                    mlog(ImgPaths[position].toString());
+                }
+                else FirstStart.setVisibility(View.VISIBLE);
+            }
+
+            @Override
+            public void onPageScrollStateChanged(int state) {
+
+            }
+
+        });
+    }
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+        if (requestCode == 1 && resultCode == RESULT_OK) {
+            List<Uri> result = Matisse.obtainResult(data);
+            Uri path = result.get(0);
+            displayImage(path,lastPosition);
+            mlog("path "+path);
+        }
+    }
+    private void mlog(String log){
+        Log.d(TAG,log);
+    }
+    private void displayImage(Uri imagePath, int position) {
+        mlog("Display image "+imagePath+" position is "+position);
+        if(imagePath != null){
+            if (imagePath!=ImgPaths[position]){
+                SharedPreferences.Editor editor = sharedPreferences.edit();//存下地址
+                editor.putString("Path"+position, imagePath.toString());
+                editor.apply();
+                ImgPaths[position] = imagePath;
+            }
+            mlog("Glide run ");
+            Glide.with(MainActivity.this)
+                    .load(imagePath)
+                    .into(view[position]);
+            FirstStart.setVisibility(View.INVISIBLE);
+        }else if(ImgPaths[position]!=null){
+            Toast.makeText(this, "Failed to load image(May be deleted)", Toast.LENGTH_SHORT).show();
+        }
     }
     private static final int REQUEST_EXTERNAL_STORAGE = 1;
     private static String[] PERMISSIONS_STORAGE = {
@@ -82,81 +185,5 @@ public class MainActivity extends AppCompatActivity {
         } catch (Exception e) {
             e.printStackTrace();
         }
-    }
-
-    //判断文件是否存在
-    public boolean fileIsExists(String strFile) {
-        try {
-            File f=new File(strFile);
-            if(!f.exists()) {
-                return false;
-            }
-        }
-        catch (Exception e) {
-            return false;
-        }
-        return true;
-    }
-
-    private void openAlbum() {
-        Intent intent = new Intent("android.intent.action.GET_CONTENT");
-        intent.setType("image/*");
-        startActivityForResult(intent, 1); // 打开相册
-        }
-
-    @Override
-    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
-        String imagePath = null;
-        Uri uri = data.getData();
-        Log.d("TAG", "handleImageOnKitKat: uri is " + uri);
-        if (DocumentsContract.isDocumentUri(this, uri)) {
-            // 如果是document类型的Uri，则通过document id处理
-            String docId = DocumentsContract.getDocumentId(uri);
-            if("com.android.providers.media.documents".equals(uri.getAuthority())) {
-                String id = docId.split(":")[1]; // 解析出数字格式的id
-                String selection = MediaStore.Images.Media._ID + "=" + id;
-                imagePath = getImagePath(MediaStore.Images.Media.EXTERNAL_CONTENT_URI, selection);
-            } else if ("com.android.providers.downloads.documents".equals(uri.getAuthority())) {
-                Uri contentUri = ContentUris.withAppendedId(Uri.parse("content://downloads/public_downloads"), Long.valueOf(docId));
-                imagePath = getImagePath(contentUri, null);
-            }
-        } else if ("content".equalsIgnoreCase(uri.getScheme())) {
-            // 如果是content类型的Uri，则使用普通方式处理
-            imagePath = getImagePath(uri, null);
-        } else if ("file".equalsIgnoreCase(uri.getScheme())) {
-            // 如果是file类型的Uri，直接获取图片路径即可
-            imagePath = uri.getPath();
-        }
-        displayImage(imagePath); // 根据图片路径显示图片
-    }
-
-    private String getImagePath(Uri uri, String selection) {
-        String path = null;
-        // 通过Uri和selection来获取真实的图片路径
-        Cursor cursor = getContentResolver().query(uri, null, selection, null, null);
-        if (cursor != null) {
-            if (cursor.moveToFirst()) {
-                path = cursor.getString(cursor.getColumnIndex(MediaStore.Images.Media.DATA));
-            }
-            cursor.close();
-        }
-        return path;
-    }
-
-    private void displayImage(String imagePath) {
-        if (imagePath != null) {
-            if(!(imagePath.equals(imgPath))){
-                SharedPreferences.Editor editor = sharedPreferences.edit();//存下地址
-                //Log.e("imgPath","Saving");
-                editor.putString("Path", imagePath);
-                editor.apply();
-            }
-            Bitmap bitmap = BitmapFactory.decodeFile(imagePath);
-            photoView.setImageBitmap(bitmap);
-            //picture.setImageBitmap(bitmap);
-        } else {
-            Toast.makeText(this, "failed to load image", Toast.LENGTH_SHORT).show();
-        }
-        FirstStart.setVisibility(View.INVISIBLE);
     }
 }
